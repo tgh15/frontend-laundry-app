ad43be39b3ccb352be05a184f1dcdcdcf23b6370
muh-teguh <muhteguhadhiputra15@gmail.com>
2020-05-30 23:17:44 +0800

    front page

--- src/Pages/Front/Landing.jsx
+++ src/Pages/Front/Landing.jsx
@@ -3,10 +3,23 @@ import React from 'react'
 //CSS
 import './Landing.css'
 
+//Section
+import HeaderSection from './HeaderSection/HeaderSection'
+import PackageSection from './PackageSection/PackageSection'
+import ContactSection from './ContactSection/ContactSection'
+import Footer from './Footer/Footer'
+import AboutSection from './AboutSection/AboutSection'
+import TrackingSection from './TrackingSection/TrackingSection'
+
 export default function Landing() {
     return (
         <div className="landing-wrap">
-            <h1>Coming Soon...</h1>
+            <HeaderSection />
+            <AboutSection />
+            <PackageSection />
+            <TrackingSection />
+            <ContactSection />
+            <Footer />
         </div>
     )
 }
